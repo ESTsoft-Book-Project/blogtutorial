2f4863a0bb68d2dd6a8f9e52621a75aaf8d2ce9d
ChoiWheatley <chltmdgus604@naver.com>
2023-05-31 04:48:06 +0000

    [fix] views.Index now shows object_list!

--- blog/models.py
+++ blog/models.py
@@ -5,14 +5,19 @@ import tinymce.models
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+
     # rich text editor HTML Field below
     content = tinymce.models.HTMLField()
+
     # automatically fill the date field with today
     date = models.DateField(auto_now_add=True)
+
     # models.CASCADE will delete all user's articles on delete account
     # DO_NOTHING, SET_NULL 등의 옵션이 있으니 참고바람.
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # as default, all articles are being not be featured
     featured = models.BooleanField(default=False)
+
     # user can hit like button
     likes = models.ManyToManyField(User, related_name="likes", blank=True)
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.views import View
 from django.views.generic import ListView
 from .models import Article
 
 # Create your views here.
 
 
-class Index(View):
+class Index(ListView):
     model = Article
     queryset = Article.objects.all().order_by("-date")
     template_name = "blog/index.html"
-
-    def get(self, request):
-        return render(request, "blog/index.html")
+    # paginate_by = 1
